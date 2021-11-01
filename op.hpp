d65f7003dea8109c15c5877bb4207874c554b36d
hbach24 <57569284+hbach24@users.noreply.github.com>
2021-11-01 15:33:05 -0700

    Update op.hpp

--- op.hpp
+++ op.hpp
@@ -2,16 +2,12 @@
 #define __OP_HPP__
 
 #include "base.hpp"
-using namespace std;
 
 class Op : public Base {
-    protected:
-	double v;
-
     public:
-        Op(double value) : Base() { v = value; }
-        virtual double evaluate() { return v; }
-        virtual std::string stringify() { return to_string(v); }
+        Op(double value) : Base() { }
+        virtual double evaluate() { return 0.0; }
+        virtual std::string stringify() { return ""; }
 };
 
 #endif //__OP_HPP__
