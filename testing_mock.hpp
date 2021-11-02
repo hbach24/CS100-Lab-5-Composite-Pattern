280134f484b92ddd92e9c69b9a1013f2c22730b5
JingwenHuang233 <jingwenhuang233@gmail.com>
2021-11-01 22:21:54 -0700

    Add testing mocks

---
+++ testing_mock.hpp
@@ -0,0 +1,28 @@
+#ifndef __TESTING_MOCK_HPP__
+#define __TESTING_MOCK_HPP__
+
+#include "base.hpp"
+class NegOp: public Base {
+    public:
+        NegOp() { }
+
+        virtual double evaluate() { return -3.5; }
+        virtual string stringify() { return "-3.5"; }
+}
+
+class PosOp: public Base {
+	public:
+		PosOp(){}
+		virtual double evaluate() {return 4.2;}
+		virtual string stringify() {return "-4.2";}
+}
+
+class ZeroOp: public Base {
+	public: 
+		ZeroOp() {}
+		virtual double evaluate() {return 0.0;}
+		virtual string evaluate() {return "0.0";}
+}
+
+
+#endif
