0d0544d55ce0aa35e3f5da1ce200ac3cf2ac36c0
JingwenHuang233 <jingwenhuang233@gmail.com>
2021-11-02 00:02:17 -0700

    Update testing mocks

--- testing_mock.hpp
+++ testing_mock.hpp
@@ -9,21 +9,21 @@ class NegOp: public Base {
 
         virtual double evaluate() { return -3.5; }
         virtual string stringify() { return "(-3.5)"; }
-}
+};
 
 class PosOp: public Base {
 	public:
 		PosOp(){}
 		virtual double evaluate() {return 4.2;}
 		virtual string stringify() {return "4.2";}
-}
+};
 
 class ZeroOp: public Base {
 	public: 
 		ZeroOp() {}
 		virtual double evaluate() {return 0.0;}
-		virtual string evaluate() {return "0.0";}
-}
+		virtual string stringify() {return "0.0";}
+};
 
 
 #endif
